3ff192e1073e73baacd6730257f4bbce90a38815
Sonali Shukla <sonalikatara@gmail.com>
2018-02-06 14:17:06 -0800

    clean:Use destructuring to clean up code

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -28,25 +28,28 @@ class ListContacts extends Component{
     }
 
     render(){
+    	const {contacts, onDeleteContact} = this.props
+    	const { query } = this.state;
+
     	let showingContacts
-    	if(this.state.query){
-    		const match = new RegExp(escapeRegExp(this.state.query), 'i')
-    		showingContacts = this.props.contacts.filter((contact)=> match.test(contact.name))
+
+    	if(query){
+    		const match = new RegExp(escapeRegExp(query), 'i')
+    		showingContacts = contacts.filter((contact)=> match.test(contact.name))
     	} else {
-    		showingContacts = this.props.contacts
+    		showingContacts = contacts
     	}
 
     	showingContacts.sort(sortBy('name'))
 
        return (
        	<div className='list-contacts'>
-       	 {JSON.stringify(this.state)}
        	 <div className='list-contacts-top'>
        	 	<input
    	 		  className='search-contacts'
    	 		  type='text'
    	 		  placeholder='Search Contacts'
-   	 		  value={this.state.query}
+   	 		  value={query}
    	 		  onChange={(event)=> this.updateQuery(event.target.value)}
        	 	/>
        	 </div>
@@ -61,7 +64,7 @@ class ListContacts extends Component{
 								<p>{contact.name}</p>
 								<p>{contact.email}</p>
 							</div>
-							<button className='contact-remove' onClick={() => this.props.onDeleteContact(contact)}>
+							<button className='contact-remove' onClick={() => onDeleteContact(contact)}>
 								Remove
 							</button>
 						</li>
