9a2f614b45013d70957e214e5da74b0cc2130117
Sonali Shukla <sonalikatara@gmail.com>
2018-02-06 21:37:33 -0800

    feat: Use componentDidMount to fetch contacts from the server

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI'
 
 // Move default contacts into App component as state
 // props refer to attributes from parent components. In the end, props represent "read-only" data that are immutable.
@@ -9,26 +10,13 @@ import ListContacts from './ListContacts';
 
 class App extends Component{
   state = {
-  contacts : [
-   {
-      "id": "ryan",
-      "name": "Ryan Florence",
-      "email": "ryan@reacttraining.com",
-      "avatarURL": "http://localhost:5001/ryan.jpg"
-    },
-    {
-      "id": "michael",
-      "name": "Michael Jackson",
-      "email": "michael@reacttraining.com",
-      "avatarURL": "http://localhost:5001/michael.jpg"
-    },
-    {
-      "id": "tyler",
-      "name": "Tyler McGinnis",
-      "email": "tyler@reacttraining.com",
-      "avatarURL": "http://localhost:5001/tyler.jpg"
-    }
-  ]
+  contacts : []
+  }
+
+  componentDidMount(){
+     ContactsAPI.getAll().then((contacts) => {
+        this.setState({ contacts })
+    })
   }
 
   removeContact = (contact) =>{
