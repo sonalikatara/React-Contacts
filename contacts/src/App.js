d67738073c476635858d7d4fad2df88ca89eb740
Sonali Shukla <sonalikatara@gmail.com>
2018-02-08 14:18:53 -0800

    feat:Save new contact to the server

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -31,8 +31,13 @@ class App extends Component{
       ContactsAPI.remove(contact)
   }
 
-  addContact = () =>{
-
+  createContact = (contact) =>{
+    ContactsAPI.create(contact).then( contact => {
+        this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+     }
+    )
   }
 
  render(){
@@ -43,7 +48,15 @@ class App extends Component{
         onDeleteContact={this.removeContact}
         contacts={this.state.contacts}/>
       )} />
-   <Route path='/create' component={CreateContact} />
+   <Route path='/create' render = {({history}) => (
+      <CreateContact
+        onCreateContact={(contact)=>{
+         this.createContact(contact)
+         history.push('/')
+      }}
+      />
+    )}
+   />
     </div>
     )
   }
